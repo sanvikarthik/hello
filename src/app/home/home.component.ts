ffe81fe0235cc22c052e39a6008cb1f754f6ad1b
sanvikarthik <sanvikarthik11@gmail.com>
2024-07-17 16:18:31 +0530

    initial commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { ProductsService } from '../services/products.service';
+import { Products } from '../../types';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+})
+export class HomeComponent {
+  constructor(private productsService: ProductsService) {}
+  ngOnInit() {
+    this.productsService
+      .getProducts('https://localhost:3000/clothes', { page: 0, perPage: 5 })
+      .subscribe((products: Products) => {
+        console.log(products.items);
+      });
+  }
+}
